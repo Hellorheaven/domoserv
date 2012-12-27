05d31d93dc42fbc9ecaf2d0cb304483ca218cb45
Hellorheaven <mickyradio@hotmail.com>
2012-12-27 16:52:20 +0100

    Update www/database/domoserv.sql

--- www/database/domoserv.sql
+++ www/database/domoserv.sql
@@ -2,10 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-
 DROP SCHEMA IF EXISTS `domoserv` ;
 CREATE SCHEMA IF NOT EXISTS `domoserv` DEFAULT CHARACTER SET utf8 ;
-
+SHOW WARNINGS;
 USE `domoserv` ;
 
 -- -----------------------------------------------------
@@ -13,6 +12,7 @@ USE `domoserv` ;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `room` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `room` (
   `room_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `room_name` VARCHAR(45) NOT NULL ,
@@ -21,12 +21,17 @@ CREATE  TABLE IF NOT EXISTS `room` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `room_name` ON `room` (`house_name` ASC, `room_name` ASC) ;
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `module`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `module` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `module` (
   `module_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `room_id` INT(11) NOT NULL ,
@@ -35,7 +40,7 @@ CREATE  TABLE IF NOT EXISTS `module` (
   `module_name` VARCHAR(45) NOT NULL ,
   `module_type` INT(3) NOT NULL ,
   `module_description` VARCHAR(100) NULL DEFAULT NULL ,
-  `notify` TINYINT(1) NULL DEFAULT '0' ,
+  `notify` TINYINT(1) NOT NULL DEFAULT '0' ,
   `notify_title` VARCHAR(100) NULL DEFAULT NULL ,
   `notify_message` VARCHAR(300) NULL DEFAULT NULL ,
   PRIMARY KEY (`module_id`) ,
@@ -47,14 +52,20 @@ CREATE  TABLE IF NOT EXISTS `module` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `module_name` ON `module` (`room_id` ASC, `zibase_id` ASC, `module_protocol` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_module_room1_idx` ON `module` (`room_id` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `battery`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `battery` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `battery` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `module_id` INT(11) NOT NULL ,
@@ -70,14 +81,20 @@ CREATE  TABLE IF NOT EXISTS `battery` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `battery` ON `battery` (`curdate` ASC, `curtime` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_battery_module1_idx` ON `battery` (`module_id` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `module_states`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `module_states` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `module_states` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `module_id` INT(11) NOT NULL ,
@@ -96,29 +113,76 @@ CREATE  TABLE IF NOT EXISTS `module_states` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `module_states` ON `module_states` (`module_id` ASC, `curdatetime` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_module_states_module1_idx` ON `module_states` (`module_id` ASC) ;
 
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `host_services`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `host_services` ;
+
+SHOW WARNINGS;
+CREATE  TABLE IF NOT EXISTS `host_services` (
+  `host_id` INT NOT NULL AUTO_INCREMENT ,
+  `host` VARCHAR(15) NOT NULL DEFAULT '0.0.0.0' ,
+  `port` TINYINT(5) NOT NULL DEFAULT '80' ,
+  `room_id` INT(11) NOT NULL ,
+  `description` VARCHAR(100) NULL ,
+  PRIMARY KEY (`host_id`) ,
+  CONSTRAINT `fk_host_services_room1`
+    FOREIGN KEY (`room_id` )
+    REFERENCES `room` (`room_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `host_services` ON `host_services` (`host` ASC, `port` ASC) ;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_host_services_room1_idx` ON `host_services` (`room_id` ASC) ;
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `ping_states`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `ping_states` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `ping_states` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `machine` VARCHAR(15) NOT NULL DEFAULT '0.0.0.0' ,
+  `host_id` INT NOT NULL ,
   `datetime` DATETIME NOT NULL ,
   `status` TINYINT(1) NOT NULL DEFAULT '0' ,
-  PRIMARY KEY (`id`) )
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_ping_states_host_services1`
+    FOREIGN KEY (`host_id` )
+    REFERENCES `host_services` (`host_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `ping` ON `ping_states` (`datetime` ASC) ;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_ping_states_host_services1_idx` ON `ping_states` (`host_id` ASC) ;
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `teleinfo_mono`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `teleinfo_mono` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `teleinfo_mono` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `timestamp` BIGINT(10) NOT NULL ,
@@ -151,14 +215,17 @@ CREATE  TABLE IF NOT EXISTS `teleinfo_mono` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
 CREATE UNIQUE INDEX `timestamp` ON `teleinfo_mono` (`timestamp` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `teleinfo_tri`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `teleinfo_tri` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `teleinfo_tri` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `timestamp` BIGINT(10) NOT NULL ,
@@ -198,33 +265,40 @@ CREATE  TABLE IF NOT EXISTS `teleinfo_tri` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
 CREATE UNIQUE INDEX `timestamp` ON `teleinfo_tri` (`timestamp` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `user` (
   `user_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `firstname` VARCHAR(45) NOT NULL ,
   `lastname` VARCHAR(45) NOT NULL ,
-  `password` VARCHAR(32) NOT NULL ,
+  `password` VARCHAR(45) NOT NULL ,
   `userlevel` VARCHAR(45) NOT NULL DEFAULT 'user' ,
   `userlatitude` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`user_id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `user` ON `user` (`username` ASC) ;
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `userhome`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `userhome` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `userhome` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `user_id` INT(11) NOT NULL ,
@@ -239,18 +313,24 @@ CREATE  TABLE IF NOT EXISTS `userhome` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `userhome` ON `userhome` (`user_id` ASC, `timestamp` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_userhome_user1_idx` ON `userhome` (`user_id` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `usernotify`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `usernotify` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `usernotify` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `user_id` INT(11) NOT NULL ,
-  `devices` VARCHAR(45) NOT NULL ,
+  `devices` VARCHAR(45) NOT NULL DEFAULT 'all' ,
   `type` VARCHAR(32) NOT NULL ,
   `contact` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -262,14 +342,20 @@ CREATE  TABLE IF NOT EXISTS `usernotify` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `usernotify` ON `usernotify` (`user_id` ASC, `devices` ASC, `type` ASC, `contact` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_usernotify_user1_idx` ON `usernotify` (`user_id` ASC) ;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `usertracking`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `usertracking` ;
 
+SHOW WARNINGS;
 CREATE  TABLE IF NOT EXISTS `usertracking` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `user_id` INT(11) NOT NULL ,
@@ -285,10 +371,24 @@ CREATE  TABLE IF NOT EXISTS `usertracking` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `usertracking` ON `usertracking` (`user_id` ASC, `timestamp` ASC) ;
+
+SHOW WARNINGS;
 CREATE INDEX `fk_usertracking_user1_idx` ON `usertracking` (`user_id` ASC) ;
 
+SHOW WARNINGS;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `domoserv`;
+INSERT INTO `user` (`user_id`, `username`, `firstname`, `lastname`, `password`, `userlevel`, `userlatitude`) VALUES (1, 'admin', 'Administrateur', 'DOMOSERV', '*F51AB0CE61F44E7DBC15CBB26966ADFCE2AEEB73', 'admin', NULL);
+
+COMMIT;
